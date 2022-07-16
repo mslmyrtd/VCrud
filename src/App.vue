21225ad3999e6b11fd4c49c5744435ea11ad2ca7
mslmyrtd <mslmyrtd@gmail.com>
2022-07-16 16:29:11 +0300

    loading added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import Navbar from "./components/Navbar.vue";
+import Loading from "./components/Loading.vue";
 </script>
 
 <template>
+<Loading/>
   <Navbar />
   <router-view></router-view>
 </template>
---
+++ src/components/Loading.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <img  class="block m-auto" src="@/assets/Infinity-1s-200px.gif" alt="">
+        </div>
+    </div>
+</div>
+</template>
