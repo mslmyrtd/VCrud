e1d258cf0a719874d5096fef33eeaaacc0f69aed
mslmyrtd <mslmyrtd@gmail.com>
2022-10-08 22:28:37 +0300

    added dynamic page titles on router

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,27 +7,41 @@ const router = createRouter({
       path: "",
       name: "home",
       component: Dashboard,
+      meta:{
+        title:"Home"
+      }
     },
     {
       path: "/editpage/:id",
       name: "EditPage",
-
       component: () => import("@/views/EditPage.vue"),
+      meta:{
+        title:"EditPage"
+      }
     },
     {
       path: "/create",
       name: "Create",
-
       component: () => import("@/views/Create.vue"),
+      meta:{
+        title:"Create"
+      }
     },
     {
       path: "/:pathMatch(.*)*",
       name: "PageNotFound",
-
       component: () => import("@/views/PageNotFound.vue"),
+      meta:{
+        title:"NotFound"
+      }
     },
   ],
 });
+
+router.beforeEach((to,from,next)=>{
+  document.title=`${to.meta.title}`;
+  next()
+})
 
 export default router;
 
